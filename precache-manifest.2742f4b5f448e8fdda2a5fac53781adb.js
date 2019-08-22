b8499d1a7db3fb306c685975f8aa9d199fd795de
MattR246 <49105348+MattR246@users.noreply.github.com>
2019-08-22 15:44:55 -0500

    Updates

---
+++ precache-manifest.2742f4b5f448e8fdda2a5fac53781adb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1889a1acb70ca3a578cb759f2a695a",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "c2394aa42e2254b26ca0",
+    "url": "/Monsters-Rolodex/static/css/main.ae1c7239.chunk.css"
+  },
+  {
+    "revision": "607e395712115653f754",
+    "url": "/Monsters-Rolodex/static/js/2.45dda9c7.chunk.js"
+  },
+  {
+    "revision": "c2394aa42e2254b26ca0",
+    "url": "/Monsters-Rolodex/static/js/main.877fd886.chunk.js"
+  },
+  {
+    "revision": "8a7f45627e3585acf36b",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.3bf5044b.js"
+  }
+]);
